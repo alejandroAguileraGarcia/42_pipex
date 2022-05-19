8e863ae94d69773db7cb4d2ae14653d7c58a5f52
Alejandro Aguilera garcia <aaguiler@c2r6s6.42malaga.com>
2022-05-19 18:20:40 +0200

    Add pipes

--- pruebas.c
+++ pruebas.c
@@ -1,1 +1,30 @@
-jh
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	int	fd[2];
+	// fd[0] for read
+	// fd[1] for write
+	int	id;
+	int	pid;
+
+	pid = pipe(fd);
+	id = fork();
+	if (id == 0)
+	{
+		close(fd[0]);
+		write(fd[1], "Z", 1);
+		close(fd[1]);
+	}
+	else
+	{
+		char	c;
+		close(fd[1]);
+		read(fd[0], &c, 1);
+		printf("%c\n", c);
+		close(fd[0]);
+	}
+
+	return (0);
+}
